5ac2ff3b860a0f9b52dd148926790d006d0f9a2c
John Doe <johndoe@example.com>
2024-05-29 17:11:35 +0800

    1

---
+++ src/Event.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPOMG\Psr14;
+
+use Fig\EventDispatcher\AggregateProvider;
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Psr\EventDispatcher\ListenerProviderInterface;
+use Psr\EventDispatcher\StoppableEventInterface;
+
+class Event extends AggregateProvider implements EventDispatcherInterface, ListenerProviderInterface
+{
+    public function dispatch(object $event)
+    {
+        if ($event instanceof StoppableEventInterface && $event->isPropagationStopped()) {
+            return $event;
+        }
+        foreach ($this->getListenersForEvent($event) as $listener) {
+            $listener($event);
+            if ($event instanceof StoppableEventInterface && $event->isPropagationStopped()) {
+                break;
+            }
+        }
+        return $event;
+    }
+}
